70d50443f7993d990fb3b8ae09e7b5ab37c4f8d8
Thomas Gebert <tombert@linux-4r4c.site>
2014-01-04 14:29:36 -0600

    Get rid of console.log and commented code.

--- controllers/User.js
+++ controllers/User.js
@@ -3,17 +3,10 @@ var render = require('co-render');
 
 module.exports = {
   find: function *(next){
-    console.log('Enter Find Function');
-    console.log(this.params.id);
     yield next; 
   },
   test: function *(next){
-    console.log('enter test function');
     this.body = yield this.render('blah', {name: 'koa'})
-    // co(function *(){
-    //   this.body = yield this.render('blah', {name: 'asdfasdf'})      
-    // })
-
     yield next; 
   }
 }
