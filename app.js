ea98669e4050468e7ba1941616d38585da2b30ec
Jordan Jenkins <satdaten@gmail.com>
2014-01-05 13:47:50 -0600

    Added a public folder for static file serving. it does not process any file, so no LESS compilation or anything like that.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var koa = require('koa');
 var app = koa();
 var logger = require('koa-logger');
 var render = require('koa-render'); 
+var serve = require('koa-static');
 var router = require('./guts/router');
 var models = require('./guts/models');
 var views = require('./guts/views'); 
 
 
 app.use(logger());
+app.use(serve(__dirname + '/public'));
 
 router(app); 
 
