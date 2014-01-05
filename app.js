587aa9859d57956da46d53f7c190d2ac6def9cde
Jordan Jenkins <satdaten@gmail.com>
2014-01-05 13:52:11 -0600

    Added session support. uses this.session to store info about a user that persists between calls.

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var app = koa();
 var logger = require('koa-logger');
 var render = require('koa-render'); 
 var serve = require('koa-static');
+var session = require('koa-session');
 var router = require('./guts/router');
 var models = require('./guts/models');
 var views = require('./guts/views'); 
 
 
 app.use(logger());
+app.keys = ['TODO move to a config file'];
+app.use(session());
 app.use(serve(__dirname + '/public'));
 
 router(app); 
